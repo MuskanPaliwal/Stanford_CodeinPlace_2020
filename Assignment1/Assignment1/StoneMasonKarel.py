e71c9c238d64727b840f1f3735bba75444510b01
Muskan Paliwal <muskan10112002@gmail.com>
2020-04-23 10:23:43 +0530

    minor bug fix in the StoneMasonKarel and made the code work in all the worlds

--- Assignment1/Assignment1/StoneMasonKarel.py
+++ Assignment1/Assignment1/StoneMasonKarel.py
@@ -8,19 +8,57 @@ solve the "repair the quad" problem from Assignment 1. You
 should make sure that your program works for all of the 
 sample worlds supplied in the starter folder.
 """
-
-
 def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
-    pass
+
+  turn_left()
+  while front_is_clear():
+     build_column()
+     descend_column()
+     """next_column()"""
+     """if front_is_blocked():
+        check_beeper()"""
+        
+"""build column function makes the karel to build a column.
+   pre-condition: beeper is facing towards west and turned left before building.
+   post-condition: front is clear, beeper build the column and front is blocked as beeper is facing towards north."""
+def build_column():
+    while front_is_clear():
+        if no_beepers_present():
+            put_beeper()
+            move()
+        else:
+            move()
+        check_beeper()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def check_beeper():
+    if no_beepers_present():
+        put_beeper()
+
+"""moves the karel downwards.
+   pre-condition: Front is blocked. Beeper is facing towards north. 
+   post-condition: Beeper is at the first row moving to the next column, 4 columns away from the first one. And take a left turn.""" 
+
+def descend_column():
+    turn_left()
+    turn_left()
+    while front_is_clear():
+        move()
+    turn_left()
+    """while right_is_blocked():"""
+    if front_is_clear():
+         for i in range(4):
+            move()
+                
+         turn_left()
+        
 
 
-# There is no need to edit code beyond this point
+  # There is no need to edit code beyond this point
 
 if __name__ == "__main__":
     run_karel_program()
